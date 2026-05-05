5eae422189f3fc84d75b9f7f08b54a245bc8d592
Anthony Marcic <aamarcic1126@vt.edu>
2026-05-05 11:28:10 -0400

    removed duplicate file

--- fpga/LiberoProject/FullSystem/FullSystem/designer/impl1/run_designer_tool.tcl
+++
@@ -1,4 +0,0 @@
-set_defvar "IDE_DESIGNERVIEW_ROOTDIR" {C:\Users\aamar\Documents\SeniorSem2\4806-MDE\FullSystem\FullSystem\designer}
-open_design fullsystem_top.adb
-open_tool mvn_ioedit
-
--- fpga/LiberoProject/FullSystem/FullSystem/synthesis/fullsystem_top_syn.tcl
+++
@@ -1,3 +0,0 @@
-project -load "C:/Users/aamar/Documents/SeniorSem2/4806-MDE/FullSystem/FullSystem/synthesis/fullsystem_top_syn.prj"
-project -run -clean
-project -save
